59eea33014c38e3c8bfd830efdf292e5e7834438
Korotkov, Maxim <gun.fever@yandex.ru>
2022-05-27 09:38:07 +0300

    cpp: ugly PigIt task solution

---
+++ cpp/codewars/PigIt.cpp
@@ -0,0 +1,37 @@
+//https://www.codewars.com/kata/520b9d2ad5c005041100000f
+#include<string>
+#include<vector>
+#include<ctype.h>
+//TODO: rewrite this solution with using of regex 
+bool isnotAlphanumeric(std::string str){
+    for(int i=0; i<str.size(); i++){
+        if( !std::isalpha(str[i]) && !std::isdigit(str[i]))
+            return true;
+    }
+    return false;
+}
+std::string pig_it(std::string str){
+    std::vector<std::string> words;
+    std::string separator = " ";
+    size_t pos = 0;
+    while ((pos = str.find(separator)) != std::string::npos) {
+        words.push_back(str.substr(0, pos));
+        str.erase(0, pos + separator.length());
+    }
+    words.push_back(str);
+    std::string result;
+    for (auto element : words) {
+        if ((element.size() == 1) && (isnotAlphanumeric(element))){
+         result.append(element+separator);   
+        }
+        else{
+            char movedChar = element[0];
+            element.erase(0,1);
+            element.push_back(movedChar);
+            result.append(element+"ay"+separator);
+        }
+    }
+    if(result.substr(result.length()-1,1) == " ") 
+        result.pop_back();
+    return result;
+}
