348a9eaf626fa88f4ed5739ff15496ed5c9436fd
Korotkov, Maxim <gun.fever@yandex.ru>
2022-07-15 13:53:49 +0300

    Cpp: refactor RGBtoHex solution

--- cpp/codewars/RGBtoHex.cpp
+++ cpp/codewars/RGBtoHex.cpp
@@ -1,22 +1,20 @@
 #include <string>
 #include <sstream>
 #include <iomanip>
+#include <algorithm>
 namespace RGBToHex
 {
-void normalize(int& var){
-    if(var>255) var = 255;
-    if(var<0) var = 0;
-}
 std::string convert_to_hex(const int& var){
     std::stringstream ss;
     ss<<std::setw(2)<<std::setfill('0')<<std::uppercase<<std::hex<<var;
     return ss.str();
 }
 std::string rgb(int r, int g, int b){
-    normalize(r);
-    normalize(g);
-    normalize(b);
-    return convert_to_hex(r) + convert_to_hex(g) + convert_to_hex(b);
+    std::string result;
+    for(auto value:{r,g,b}){
+        result.append(convert_to_hex(std::clamp(value,0,255)));
+    } 
+    return result;
 }
 };
 
