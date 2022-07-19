63ffe63e42ec10592320ee4672e5b6e1b6fe87de
Korotkov, Maxim <gun.fever@yandex.ru>
2022-07-19 16:20:37 +0300

    cpp: refactor solution

--- cpp/codewars/MovingZerosToTheEnd2.cpp
+++ cpp/codewars/MovingZerosToTheEnd2.cpp
@@ -1,9 +1,6 @@
 #include <vector>
 std::vector<int> move_zeroes(const std::vector<int>& input) {
-std::vector<int> output;
-for (auto elem:input){
-  if(elem != 0) output.push_back(elem);
-}
-  output.resize(input.size());
+  std::vector<int> output(input.size(),0);
+  std::copy_if(input.begin(), input.end(), output.begin(), [](int i){ return i != 0; });
   return output;
 }
