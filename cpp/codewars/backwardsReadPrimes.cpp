0b855779606339a37366766489fc28d118b77610
Korotkov, Maxim <korotkov.maxim.s@gmail.com>
2022-11-09 16:55:37 +0300

    Code style fix

--- cpp/codewars/backwardsReadPrimes.cpp
+++ cpp/codewars/backwardsReadPrimes.cpp
@@ -10,11 +10,11 @@ public:
     bool prime, backwardsPrime, notSame;
     const auto sep = " ";
     for(auto i = start;i<=end;i++){
-      auto reversed_item =  reverse_item(i);
+      auto reversed_item = reverse_item(i);
       prime = isPrime(i);
       backwardsPrime = isPrime(reversed_item);
       notSame = i != reversed_item;
-      if(prime&&notSame&&backwardsPrime){
+      if(prime && notSame && backwardsPrime){
        res += std::to_string(i) + sep; 
       }
     }
