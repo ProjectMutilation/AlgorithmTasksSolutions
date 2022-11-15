f4f7fe6550285005ed6fd1f6aefc12b7cd2ab1d3
Korotkov, Maxim <korotkov.maxim.s@gmail.com>
2022-11-15 09:18:17 +0300

    simplifying solution

--- cpp/codewars/int32toip.cpp
+++ cpp/codewars/int32toip.cpp
@@ -1,16 +1,17 @@
 //https://www.codewars.com/kata/52e88b39ffb6ac53a400022e/
 #include <sstream>
 #include <iomanip>
-#include <iostream>
 #include <bitset>
+
 std::string uint32_to_ip(uint32_t ip){
     std::stringstream ss;
     auto const separator = ".";
-    std::bitset<32> bits = std::bitset<32>(ip);
-    auto fourth_octet = ((bits&std::bitset<32>(0x000000FF)) >>  0).to_ulong();
-    auto third_octet  = ((bits&std::bitset<32>(0x0000FF00)) >>  8).to_ulong();
-    auto second_octet = ((bits&std::bitset<32>(0x00FF0000)) >> 16).to_ulong();
-    auto first_octet  = ((bits&std::bitset<32>(0xFF000000)) >> 24).to_ulong();
+    auto bits = std::bitset<32>(ip);
+    auto const mask = std::bitset<32>(0xFF);
+    auto fourth_octet =  (bits        & mask).to_ulong();
+    auto third_octet  = ((bits >> 8)  & mask).to_ulong();
+    auto second_octet = ((bits >> 16) & mask).to_ulong();
+    auto first_octet  = ((bits >> 24) & mask).to_ulong();
     ss<<first_octet<<separator<<second_octet<<separator<<third_octet<<separator<<fourth_octet;
     return ss.str();
 }
