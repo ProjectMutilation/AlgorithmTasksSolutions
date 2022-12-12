f25956a6a67e86000b88e523924b82f1a5aed08d
Maxim Korotkov <korotkov.maxim.s@gmail.com>
2022-12-12 17:27:15 +0300

    a liitle changes of formatting

--- cpp/codewars/snailSort.cpp
+++ cpp/codewars/snailSort.cpp
@@ -1,47 +1,56 @@
 #include <string>
 #include <vector>
 #include<iostream>
+
 void rightSlice(std::vector<std::vector<int>>&snail,std::vector<int> &result){
-    for(size_t i = 0;i < snail[0].size();i++){
+    for(int i = 0; i < snail[0].size(); i++){
         result.push_back(snail[0][i]);
     }
+
     snail.erase(snail.begin(),snail.begin()+1);
-};
+}
+
 void leftSlice(std::vector<std::vector<int>>&snail,std::vector<int> &result){
-    for(size_t i = snail[snail.size()-1].size()-1;;i--){
+    for(int i = snail[snail.size()-1].size()-1; i >= 0; i--){
         result.push_back(snail[snail.size()-1][i]);
-        if(!i) break;
     }
+
     snail.erase(snail.end()-1);
-};
+}
 
 void downSlice(std::vector<std::vector<int>>&snail,std::vector<int> &result){
-    for (size_t i = 0; i < snail.size(); i++){
+    for (int i = 0; i < snail.size(); i++){
         result.push_back(snail[i][snail[i].size()-1]);        
     }
-    for (size_t i = 0; i < snail.size(); i++)
-        snail[i].erase(snail[i].end()-1);
-};
-void upSlice(std::vector<std::vector<int>>&snail,std::vector<int> &result){
-    for (size_t i = snail.size() - 1; ; i--){
-        result.push_back(snail[i][0]);
-        if(!i) break;        
-    }
-    for (size_t i = 0; i < snail.size(); i++)
-        snail[i].erase(snail[i].begin());
     
+    for (int i = 0; i < snail.size(); i++)
+        snail[i].erase(snail[i].end()-1);
 }
+
+void upSlice(std::vector<std::vector<int>>&snail,std::vector<int> &result){
+    for (int i = snail.size() - 1;i >= 0 ; i--){
+        result.push_back(snail[i][0]);               
+    }
+
+    for (int i = 0; i < snail.size(); i++) {
+        snail[i].erase(snail[i].begin());
+    }
+}
+
 std::vector<int> snail(const std::vector<std::vector<int>> &snail_map) {
   std::vector<std::vector<int>> snail = snail_map;
   std::vector<int> result;
-  while(true){
-   if(snail.empty()) break;
+  while(!snail.empty()) {
+
    rightSlice(snail,result);
-   if(snail.empty()) break;
+   if(snail.empty()) { break; }
+   
    downSlice(snail,result);
-   if(snail.empty()) break;
-   leftSlice(snail,result);
-   if(snail.empty()) break;
+   if(snail.empty()) { break; }
+
+   leftSlice(snail,result);   
+   if(snail.empty()) { break; }
+
    upSlice(snail,result);
     
   }
