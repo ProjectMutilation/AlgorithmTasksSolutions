57099340ebf960b0977a7f754d70e7a502608591
Maxim Korotkov <korotkov.maxim.s@gmail.com>
2023-02-20 13:48:46 +0300

    fix style

--- python/codewars/valid_parentheses.py
+++ python/codewars/valid_parentheses.py
@@ -2,19 +2,19 @@
 # Write a function that takes a string of parentheses, and determines if the order of the parentheses is valid. 
 
 def valid_parentheses(string):
-    if (string.count('(') != string.count(')')): 
+    if string.count('(') != string.count(')'):
         return False
     stack = []
     for s in string:
-        if(s == '('):
+        if s == '(':
             stack.append(s)
-        elif(s == ')'):
+        elif s == ')':
             try:
                 stack.pop()
             except IndexError:
                 return False
 
-    if(len(stack) == 0):
+    if len(stack) == 0:
         return True
     else:
         return False
