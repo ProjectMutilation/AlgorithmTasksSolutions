75011a931158bcd9cc624f1e34b213d5b771587f
Maxim Korotkov <korotkov.maxim.s@gmail.com>
2024-02-16 12:30:59 +0300

    refactoring wovelCode

--- cpp/codewars/WovelCode.cpp
+++ cpp/codewars/WovelCode.cpp
@@ -1,27 +1,37 @@
 //https://www.codewars.com/kata/53697be005f803751e0015aa
-//write finction to replace all the lowercase vowels in a given string with numbers and function to turn to back
+//write function to replace all the lowercase vowels in a given string with numbers and function to turn to back
 #include <string>
-#include <iostream>
 #include <algorithm>
+#include <unordered_map>
 
+std::unordered_map<char, char> replacements = {
+        {'a', 1},
+        {'e', 2},
+        {'i', 3},
+        {'o', 4},
+        {'u', 5},
+        {1, 'a'},
+        {2, 'e'},
+        {3, 'i'},
+        {4, 'o'},
+        {5, 'u'}
+};
 
 std::string encode(const std::string &str) {
-   char encoded_symbol = 49; //49 is ascii code of digit "1"
-   std::string res = str;
-   for (auto symbol : {'a','e','i','o','u'}){
-        std::replace(res.begin(), res.end(), symbol, encoded_symbol);
-        encoded_symbol++;
-   }
-   return res;
+   return impl(str, replacements);
 }
 
 
 std::string decode(const std::string &str) {
-   char decoded_symbol = 49;
-   std::string res = str;
-   for (auto symbol : {'a','e','i','o','u'}){
-        std::replace(res.begin(), res.end(), decoded_symbol,  symbol);
-        decoded_symbol++;
-   }
-   return res;
+   return impl(str, replacements);
 };
+
+std::string impl(const std::string& str, const std::unordered_map<char, char>& repl) {
+   std::string output{str};
+   std::for_each(output.begin(), output.end(), [&repl](char& ch) {
+      if (repl.find(ch) != repl.end()) {
+         ch = repl.at(ch);
+      }
+   });
+   return output;
+}
