41f251a85fb481a550207252afadd78ee1dfc99f
Maxim Korotkov <korotkov.maxim.s@gmail.com>
2024-02-16 10:24:33 +0300

    refactoring solution

--- cpp/leetcode/path_crossing.hpp
+++ cpp/leetcode/path_crossing.hpp
@@ -3,38 +3,41 @@
 #include <utility>
 #include <vector>
 #include <algorithm>
+#include <set>
+#include <string>
 
 class Solution {
 public:
-    struct Point{
-    int x;
-    int y;
+    struct Point {
+        int x;
+        int y;
     };
 
     bool isPathCrossing(std::string path) {
     Point pos {0, 0};
-    std::vector<std::pair<int, int>>visited;
-    visited.push_back(std::make_pair(0,0));
-    for (auto step: path){
+    std::set<std::pair<int, int>> visited;
+    visited.insert(std::make_pair(0,0));
+    for (const auto &step: path) {
         switch (step) {
             case 'N': 
-                pos.y++; break;
+                pos.y++;
+                break;
             case 'S':
-                pos.y--; break;
+                pos.y--;
+                break;
             case 'E':
-                pos.x++; break;
+                pos.x++;
+                break;
             case 'W':
-                pos.x--; break;
+                pos.x--;
+                break;
             default:
                 break;
         }
-        auto curr_point = std::make_pair(pos.x,pos.y);
-        auto it = std::find(visited.begin(),visited.end(), curr_point);
-        if (it != visited.end())
-            return true;
-        visited.push_back(curr_point);
+        const auto currentPointPos = std::make_pair(pos.x, pos.y);
+        const auto [it, success] = visited.insert(currentPointPos);
+        if (!success) { return true; }
     }
-
     return false;
     }
 };
