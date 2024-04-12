6162d356e46183ed73a4614434d4aec63451aa68
Maxim Korotkov <korotkov.maxim.s@gmail.com>
2024-04-12 14:29:50 +0300

    refactored RGB to hex solution
    Signed-off-by: Maxim Korotkov <korotkov.maxim.s@gmail.com>

--- cpp/codewars/RGBtoHex.cpp
+++ cpp/codewars/RGBtoHex.cpp
@@ -2,19 +2,19 @@
 #include <sstream>
 #include <iomanip>
 #include <algorithm>
-namespace RGBToHex
-{
-std::string convert_to_hex(const int& var){
-    std::stringstream ss;
-    ss<<std::setw(2)<<std::setfill('0')<<std::uppercase<<std::hex<<var;
+#include <array>
+
+std::string convert_to_hex(const int& value) {
+    std::ostringstream ss;
+    ss << std::setw(2) << std::setfill('0') << std::uppercase<<std::hex << value;
     return ss.str();
 }
-std::string rgb(int r, int g, int b){
-    std::string result;
-    for(auto value:{r,g,b}){
-        result.append(convert_to_hex(std::clamp(value,0,255)));
+std::string rgb_to_hex(const int &r, const int &g, const int &b) {
+    const std::array<int, 3> colorCodes = {std::clamp(r, 0, 255), std::clamp(g, 0, 255), std::clamp(b, 0, 255)};
+    std::string color;
+    color.reserve(colorCodes.size() * 2);
+    for (const auto &colorCode : colorCodes) {
+        color.append(convert_to_hex(colorCode));
     } 
-    return result;
+    return color;
 }
-};
-
