734e7171f27cedf46beb3b6a9ce09bfccf793c7c
Maxim Korotkov <korotkov.maxim.s@gmail.com>
2024-09-23 16:16:21 +0300

    refactored solution

--- cpp/codewars/Beeramid.cpp
+++ cpp/codewars/Beeramid.cpp
@@ -1,11 +1,7 @@
 //https://www.codewars.com/kata/51e04f6b544cf3f6550000c1
-int beeramid(int bonus, double price)
-{
-  double doubledBonus = static_cast<double>(bonus);
-  int counter = 0;
-  while ((doubledBonus - price * (counter+1)*(counter+1)) >= 0) {
-            ++counter;
-            doubledBonus -= (price * (counter)*(counter));
-           }              
-    return counter;
+#include <cmath>
+int beeramid(int bonus, double price) {
+  const unsigned int beer_count = bonus > 0 ? bonus / price : 0;
+  const unsigned int n = std::cbrt(3 * beer_count); //approximation
+  return beer_count >= n * (n + 1) * (2 * n + 1) /6 ? n : n - 1;
 }
